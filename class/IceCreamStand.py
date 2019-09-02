cc2ece02d6624e48a69013702cd99da5742873f2
alex233376 <alex233376@gmail.com>
2019-09-02 08:02:39 +0300

    Update IceCreamStand.py

--- class/IceCreamStand.py
+++ class/IceCreamStand.py
@@ -1,4 +1,5 @@
 class Restaurant():
+    
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
@@ -18,5 +19,16 @@ restaurant.open_restaurant()
 
 
 class IceCreamStand(Restaurant):
-    def __init__(self, flavors):
-        self.flavors = flavors
+    """Выводит список мороженного"""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ['mal', 'klub', 'zer']
+
+    def describe_flavors(self):
+        print("Мороженное " + str(self.flavors))
+
+
+icecream = IceCreamStand('ptz', 'figu')
+print(icecream.describe_restaurant())
+icecream.describe_flavors()
