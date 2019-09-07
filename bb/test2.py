34f21d68ecbfd55f5dac6e4d0b068c5a72fe50e6
alex233376 <alex233376@gmail.com>
2019-09-07 09:42:35 +0300

    1

--- bb/test2.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-
-r = sr.Recognizer()
-with sr.Microphone(device_index=16) as source:
-    print("Скажите что нибудь ...")
-    audio = r.listen(source)
-
-queri = r.recognize_google(audio, language="ru-RU")
-print("Вы сказали: " + queri.lower())
