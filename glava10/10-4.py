4693025343b96d9b1c26b87a1a17deb7ebc23b0b
alex233376 <alex233376@gmail.com>
2019-10-17 05:36:08 +0300

    all

--- glava10/10-4.py
+++ glava10/10-4.py
@@ -1,7 +1,8 @@
-user_name = input('Введите своё имя\n')
-print(user_name, 'Hello')
-#while user_name:
-filename = 'D:/Project/glava10/guest_book.txt'
-with open(filename, 'a') as file_object:
-    file_object.write( user_name )
-    file_object.write(' Hello\n')
+while True:
+    user_name = input("\nEnter name ")
+    print('Hello', user_name)
+
+    filename = 'D:/Project/glava10/guest_book.txt'
+    with open(filename, 'a') as file_object:
+        file_object.write(user_name)
+        file_object.write(' Hello\n')
