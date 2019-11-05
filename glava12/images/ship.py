256550f1f292bf01a38f90c019b3e578daaed579
alex233376 <alex233376@gmail.com>
2019-11-05 06:09:09 +0300

    all

--- glava12/images/ship.py
+++ glava12/images/ship.py
@@ -1,20 +1,16 @@
 import pygame
  
 class Ship:
-    """Класс для управления кораблем."""
- 
-    def __init__(self, ai_game):
+    def __init__(self, screen):
         """Инициализируйте корабль и установите его начальную позицию."""
-        self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
-
+        self.screen = screen
         # Загрузите изображение корабля и получите его прямоугольник.
         self.image = pygame.image.load('D:/Project/glava12/images/ship.bmp')
         self.rect = self.image.get_rect()
-
+        self.screen_rect = screen.get_rect()
         # Начните каждый новый корабль в центре нижней части экрана.
-        self.rect.midbottom = self.screen_rect.midbottom
-
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
     def blitme(self):
         """Нарисуйте корабль в его текущем местоположении."""
         self.screen.blit(self.image, self.rect)
--- glava12/settings.py
+++
@@ -1,9 +0,0 @@
-class Settings:
-    """Класс для хранения всех настроек для Alien Invasion."""
-
-    def __init__(self):
-        """Инициализировать настройки игры."""
-        # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (30, 43, 43)
