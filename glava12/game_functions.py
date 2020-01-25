bcbdb1f6fbb0056e6a3007301bf5a430a1cf2e01
alex233376 <alex233376@gmail.com>
2020-01-25 08:53:37 +0300

    25.01

--- glava12/game_functions.py
+++ glava12/game_functions.py
@@ -1,20 +1,26 @@
 import sys
 import pygame
+def check_keydown_events(event, ship):
+    """Реагирует на нажатия клавиш"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+def check_keyup_events(event, ship):
+    """Реагирует на отпускание клавиш."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 def check_events(ship):
     """Обрабатывает нажатия клавиш и события мыши."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.KEYDOWN:#проверка нажатия клавиши со стрелками
-            if event.key == pygame.K_RIGHT:#кнопка нажата двигаемся в право
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:#кнопка нажата двигаемся в лево
-                ship.moving_left = True
-        elif event.type == pygame.KEYUP:# проверка что кнопка отпущенна
-            if event.key == pygame.K_RIGHT:#кнопка отпущенна остановились
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:#кнопка отпущенна остановились
-                ship.moving_left = False
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
