faff3cbdd45d8af426113c6b071f72cb694eb227
alex233376 <alex233376@gmail.com>
2020-05-12 19:55:15 +0300

    12.05

--- proj/glava9/upr9.5.py
+++ proj/glava9/upr9.5.py
@@ -3,6 +3,7 @@ class User():
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attempts = 0
 
     def describe_user(self):
         print(
@@ -11,7 +12,17 @@ class User():
     def greet_user(self):
         print(f"Привет {self.first_name.title()} {self.last_name.title()} ")
 
+    def increment_login_attempts(self):
+        self.login_attempts += 5
+        print(f"Залогинился {self.login_attempts} раз")
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print(f"Залогинился {self.login_attempts} раз")
+
 
 users = User('ivan', 'ivanov')
 users.describe_user()
 users.greet_user()
+users.increment_login_attempts()
+users.reset_login_attempts()
