cf46df65ff2fb64da2b425e0354f3823db059234
alex233376 <alex233376@gmail.com>
2020-05-14 21:00:35 +0300

    14.05

--- proj/glava9/upr9.15.py
+++ proj/glava9/upr9.15.py
@@ -2,8 +2,17 @@ import random
 from random import randint
 # выборка с заменой
 rand = [2, 12, 7, 4, 3, 11, 15]
+test_ticket = []
 # Выбираем случайное количество значений из списка
 # Без повторений random.sample
 # Можно random.choices тогда повторяются
 win_ticket = random.choices(rand, k=2)
 print("Эта комбинация выиграла", win_ticket)
+rand1 = [2, 12, 7, 4, 3, 11, 15]
+
+while rand1:
+    test = random.choices(rand1, k=2)
+    print(test)
+    if test == win_ticket:
+        print('Вот она', test)
+        break
