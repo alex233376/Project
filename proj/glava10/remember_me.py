687769315be98fc8b101f6501bf3c024031b18e6
alex233376 <alex233376@gmail.com>
2020-05-17 09:58:10 +0300

    17ю05

--- proj/glava10/remember_me.py
+++ proj/glava10/remember_me.py
@@ -1,9 +1,15 @@
 import json
 
-username = input('Как ваше имя ')
-
+# Программа загружает имя пользователя, если оно было сохранено ранее.
+#  В противном случае она запрашивает имя пользователя и сохраняет его.
 filename = 'username.json'
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f'Мы будем помнить тебя, когда ты вернешься {username}')
-    
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("Введите ваше имя ")
+    with open(filename, 'w') as f:
+        json.dump(username, f)  # сохраняем данные пользователя
+        print(f"Мы будем помнить тебя, когда ты вернешься {username}")
+else:
+    print(f'С возвращением {username}')
