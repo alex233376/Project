7764e41fc4af0e8827dbfbd29a7db0491083e237
alex233376 <alex233376@gmail.com>
2020-05-21 18:50:05 +0300

    21.05

--- proj/alien_invasion/alien_invasion.py
+++ proj/alien_invasion/alien_invasion.py
@@ -32,12 +32,11 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
-        """Обрабатывает нажатия клавиш и события мыши."""
-
-        # Отслеживание событий клавиатуры и мыши.
+        """Реагировать на нажатия клавиш и события мыши."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -47,7 +46,7 @@ class AlienInvasion:
                 self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
-        """Реагирует на нажатие клавиш."""
+        """Реагирует на нажатия клавиш."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
@@ -58,64 +57,90 @@ class AlienInvasion:
             self._fire_bullet()
 
     def _check_keyup_events(self, event):
+        """Реагирует на ртпускание клавиш."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
     def _fire_bullet(self):
-        """Создание нового снаряда и включение его в группу bullets."""
+        """Создайте новую пулю и добавьте ее в группу пуль."""
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
-            # Метод add() похож на append(), но этот
             self.bullets.add(new_bullet)
-        # метод написан специально для групп Pygame
 
     def _update_bullets(self):
-        """Обновляет позиции снарядов и уничтожает старые снаряды."""
-        # Обновление позиций снарядов.
+        """Обновите положение пуль и избавьтесь от старых пуль."""
+        # Update bullet positions.
         self.bullets.update()
-        # Удаление снарядов, вышедших за край экрана
-        for bullet in self .bullets.copy():
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """
+        Проверьте, находится ли флот на краю,
+        затем обновите позиции всех пришельцев во флоте.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
-        """Создание флота вторжения."""
-        # Создание пришельца и вычисление количества пришельцев в ряду
-        # Интервал между соседними пришельцами равен ширине пришельца.
-        alien = Alien(self)  # создаём пришельца
-        # ширина пришельца определяется по его
-        # атрибуту rect, а полученное значение сохраняется в alien_width
-        alien_width = alien.rect.width
-        #  вычисляется доступное горизон тальное
-        # пространство и количество пришельцев, которые в нем поместятся
+        """Создание флота."""
+        # Создайте пришельца и найдите количество пришельцев в ряду.
+        # Расстояние между каждым пришельцем равно одной ширине пришельца.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
-        # создается цикл от 0 до количества создаваемых пришельцев
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
 
-    def _create_alien(self, alien_number):
-        "Создание пришельца и размещение его в ряду."
+        # Определите количество рядов пришельцев, которые помещаются на экране.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Создайте полный флот пришельцев.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Создайте инопланетянина и поместите его в ряд."""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление флота."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_screen(self):
-        # При каждом проходе цикла перерисовывается экран.
+        """Обновите изображения на экране и перейдите на новый экран."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
-        # Отображение последнего прорисованного экрана.
+
         pygame.display.flip()
 
 
-if __name__ == "__main__":
-    # Создание экземпляра и запуск игры.
+if __name__ == '__main__':
+    # Сделайте экземпляр игры и запустите игру.
     ai = AlienInvasion()
     ai.run_game()
--- proj/alien_invasion/ship.py
+++ proj/alien_invasion/ship.py
@@ -1,44 +1,40 @@
 import pygame
 
 
-class Ship():
-    """Класс для управления кораблем."""
+class Ship:
+    """A class to manage the ship."""
 
     def __init__(self, ai_game):
-        """Инициализирует корабль и задает его начальную позицию."""
-        # экран присваивается атрибуту Ship, чтобы к нему можно
-        # было легко обращаться во всех модулях класса
+        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        # обращается к атрибуту rect позволяет разместить корабль
-        # в нужной позиции экрана.
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Загружает изображение корабля и получает прямоугольник.
+        # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-        # Каждый новый корабль появляется у нижнего края экрана.
-        # self.rect.midbottom выравнивается по атрибуту midbottom
-        # прямоугольника зкрана
+
+        # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
-        # Сохранение вещественной координаты центра корабля.
+
+        # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
 
-        # Флаг перемещения
+        # Movement flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """Обновляет позицию корабля с учетом флага."""
-        # Обновляется атрибут x, не rect.
+        """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-        # Обновление атрибута rect на основании self.x.
+
+        # Update rect object from self.x.
         self.rect.x = self.x
 
     def blitme(self):
-        """Рисует корабль в текущей позиции."""
+        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
