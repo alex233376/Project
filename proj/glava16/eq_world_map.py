306abf1b5b52c7696b30417f859e04625dd9f211
alex233376 <alex233376@gmail.com>
2020-05-25 12:58:53 +0300

    25

---
+++ proj/glava16/eq_world_map.py
@@ -0,0 +1,23 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+# Изучение структуры данных.
+filename = 'glava16/data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+all_eq_dicst = all_eq_data['features']
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicst:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+# Нанесение данных на карту.
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
