d0b74930e692da13a0b1feb393fc2e139856ad8f
alex233376 <alex233376@gmail.com>
2020-05-25 13:31:10 +0300

    25

--- proj/glava16/eq_world_map.py
+++ proj/glava16/eq_world_map.py
@@ -8,19 +8,22 @@ filename = 'glava16/data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 all_eq_dicst = all_eq_data['features']
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicst:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 # Нанесение данных на карту.
 data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],
         'color': mags,
