8826d692ebe745e5e793e2df91b7b16c4297167e
alex233376 <alex233376@gmail.com>
2020-05-25 10:50:24 +0300

    25

--- proj/glava16/eq_explore_data.py
+++ proj/glava16/eq_explore_data.py
@@ -4,8 +4,14 @@ filename = 'glava16/data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 all_eq_dicst = all_eq_data['features']
-mags = []
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicst:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
