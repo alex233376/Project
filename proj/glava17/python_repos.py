02d8d77a2ca30e32fd0e50a6f6c296afae8f032b
alex233376 <alex233376@gmail.com>
2020-05-25 20:16:36 +0300

    25

--- proj/glava17/python_repos.py
+++ proj/glava17/python_repos.py
@@ -12,16 +12,19 @@ print(f"Все репозитории: {reesponse_dict['total_count']}")
 repo_dicts = reesponse_dict['items']
 print(f'Найденные репозитории: {len(repo_dicts)}')
 
-# Анализ первого репозитория.
-repo_dict = repo_dicts[0]
-# print(f'\nКлюч: {len(repo_dict)}')
-# for key in sorted(repo_dict.keys()):
-#     print(key)
-print("\nСобираем инфу из первого репозитория ")
-print(f"Имя: {repo_dict['name']}")
-print(f"Владелец: {repo_dict['owner']['login']}")
-print(f"Звёзды: {repo_dict['stargazers_count']}")
-print(f"Репозиторий: {repo_dict['html_url']}")
-print(f"Создан: {repo_dict['created_at']}")
-print(f"Обновлён: {repo_dict['updated_at']}")
-print(f"Описание: {repo_dict['description']}")
+print("\nНайденная инфа о каждом репозитории:")
+for repo_dict in repo_dicts:
+
+    # Анализ первого репозитория.
+    # repo_dict = repo_dicts[0]
+    # print(f'\nКлюч: {len(repo_dict)}')
+    # for key in sorted(repo_dict.keys()):
+    #     print(key)
+    print("\nСобираем инфу о репозитории ")
+    print(f"Имя: {repo_dict['name']}")
+    print(f"Владелец: {repo_dict['owner']['login']}")
+    print(f"Звёзды: {repo_dict['stargazers_count']}")
+    print(f"Репозиторий: {repo_dict['html_url']}")
+    print(f"Создан: {repo_dict['created_at']}")
+    print(f"Обновлён: {repo_dict['updated_at']}")
+    print(f"Описание: {repo_dict['description']}")
