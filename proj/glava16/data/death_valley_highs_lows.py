e3bc4ee67939d2154513164a1205a81b7b40eec5
alex233376 <alex233376@gmail.com>
2020-05-25 08:41:17 +0300

    25.05

--- proj/glava16/data/death_valley_highs_lows.py
+++ proj/glava16/data/death_valley_highs_lows.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-filename = 'glava16/data/sitka_weather_2018_simple.csv'
+filename = 'glava16/data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -11,11 +11,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Отсутствует дата {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 # Нанесение данных на диаграмму.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
@@ -24,7 +28,8 @@ ax.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.3)
 
 # Форматирование диаграммы
-plt.title("Daily high temperatures - 2018", fontsize=24)
+title = "Daily high temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
--- proj/glava7/parrot.py
+++ proj/glava7/parrot.py
@@ -2,6 +2,6 @@ prompt = input("\nСкажи мне что-нибудь, и я повторю э
 prompt += "\nВведите 'quit' для выхода из программы:"
 message = ""
 while message != 'quit':
-	message = input(prompt)
-	if message != 'quit':  # Убираем quit из вывода
-		print(message)
+    message = input(prompt)
+    if message != 'quit':  # Убираем quit из вывода
+        print(message)
